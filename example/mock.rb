cc6959bd99df021efaae40ad8661433e1b1f190f
Vishal <vishalgarg231@gmail.com>
2016-10-09 22:08:09 +0530

    Corrected example

--- example/mock.rb
+++ example/mock.rb
@@ -11,9 +11,9 @@ end
 # Find Existing user or you may create new user
 user = User.find 'f854208e-3fc9-4bf7-b9c9-dad46c596ed6'
 # Give image name (optional)
-image.picture_data_filename = 'somefile.jpg'
+user.picture_data_filename = 'somefile.jpg'
 # Give image mimetype (optional)
-image.picture_data_mimetype = 'image/jpeg'
+user.picture_data_mimetype = 'image/jpeg'
 # Assign image data uri
 user.picture_data_uri = 'data:image/gif;base64,R0lGODlhAQABAJEAAAAAAP////8AAP///yH5BAEAAAMALAAAAAABAAEAAAICVAEAOw=='
 user.save
